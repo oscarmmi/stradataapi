ac7485c0e5446bdbdf9e6e0c53d164a3522c3b5a
Oscar Muñoz <oscarxxi@gmail.com>
2021-12-21 23:29:51 -0500

    api jwt terminado

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,43 +2,41 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
-        'password',
+        'email_verified_at',
+        'password'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    // Rest omitted for brevity
 
     /**
-     * The attributes that should be cast.
+     * Get the identifier that will be stored in the subject claim of the JWT.
      *
-     * @var array<string, string>
+     * @return mixed
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -13,11 +13,16 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('auth/login', [\App\Http\Controllers\AuthController::class, 'login'])->name('login');
+Route::group([
+    'middleware' => 'jwt.verify',
+    'prefix' => 'auth'
+], function ($router) {    
+    Route::post('logout', [\App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
+    Route::post('refresh', [\App\Http\Controllers\AuthController::class, 'refresh'])->name('refresh');
+    Route::post('me', [\App\Http\Controllers\AuthController::class, 'me'])->name('me');
+    Route::post('busquedaCoincidencias', [\App\Http\Controllers\busquedaCoincidenciasController::class, 'index'])->name('busquedaCoincidencias');
+    Route::post('logCoincidencias', [\App\Http\Controllers\logCoincidenciasController::class, 'index'])->name('logCoincidencias');    
 });
 
-Route::post('/busquedaCoincidencias', 'App\Http\Controllers\busquedaCoincidenciasController@index');
 
-Route::post('/logCoincidencias', 'App\Http\Controllers\logCoincidenciasController@index');
